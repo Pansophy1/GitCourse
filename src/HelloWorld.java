c44c91a7ae256dcba57e09f7903273d9257a1200
Guest User <IanHughes1@gmail.com>
2014-05-29 12:47:24 +0100

    removed error comments

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,11 +3,9 @@ public class HelloWorld {
 // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
 	System.out.println ("Hello My World!");
-<<<<<<< HEAD
     System.out.println ("Hello your World");
 	System.out.println ("Hello World!");
-=======
     System.out.println ("Hellow your World");
->>>>>>> parent of 866fe9a... removed W
+    System.out.println ("Hellow your World");
     }
 }
