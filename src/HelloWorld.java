e0f7890ef76e6bdfc6cb2484ca2ef916a65bf910
Guest User <IanHughes1@gmail.com>
2014-05-29 12:56:11 +0100

    deleted lines in feature branch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,6 @@ public class HelloWorld {
 
 // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-	System.out.println ("Hello My World!");
-    System.out.println ("Hello your World");
 	System.out.println ("Hello World!");
     }
 }
