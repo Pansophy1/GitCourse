f1f8ebcde103f19b505947ee9ac3493856605a30
Guest User <IanHughes1@gmail.com>
2014-05-29 12:09:59 +0100

    lines taken out

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,9 @@
 public class HelloWorld {
 
-    // method main(): ALWAYS the APPLICATION entry point
+// method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-// <<<<<<< HEAD
 	System.out.println ("Hello My World!");
     System.out.println ("Hello your World");
-// =======
 	System.out.println ("Hello World!");
-// >>>>>>> parent of 12e98f4... Changed to my world
     }
 }
