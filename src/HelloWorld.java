a1ce6cca68427111dc6c34ff83df8bd96cbe8e8e
Guest User <IanHughes1@gmail.com>
2014-05-29 12:40:07 +0100

    no message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,11 @@ public class HelloWorld {
 // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
 	System.out.println ("Hello My World!");
+<<<<<<< HEAD
     System.out.println ("Hello your World");
 	System.out.println ("Hello World!");
+=======
+    System.out.println ("Hellow your World");
+>>>>>>> parent of 866fe9a... removed W
     }
 }
